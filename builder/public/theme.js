2cc4013cf0e4d45bab01856cd55f690bcfb84753
yaminuchiha1245-png <yaminuchiha1245@gmail.com>
2026-08-08 13:15:36 +0300

    chore(theme): load storefront precision release 24

--- builder/public/theme.js
+++ builder/public/theme.js
@@ -1,8 +1,8 @@
 (function () {
   "use strict";
 
-  var RELEASE = "2026.08.08.23-color";
-  var ASSET_VERSION = "2026.08.08.23";
+  var RELEASE = "2026.08.08.24-color";
+  var ASSET_VERSION = "2026.08.08.24";
   var storageKey = "uchiha-ui-theme";
   var root = document.documentElement;
   var media = typeof window.matchMedia === "function"
