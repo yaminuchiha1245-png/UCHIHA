6e20198344bad122b0a45f87ee2f106753c73189
yaminuchiha1245-png <yaminuchiha1245@gmail.com>
2026-08-08 13:15:32 +0300

    chore(pwa): bump release to 24

--- builder/public/pwa.js
+++ builder/public/pwa.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Service-worker registration is intentionally owned by this file only.
-  const RELEASE_VERSION = "2026.08.08.23";
+  const RELEASE_VERSION = "2026.08.08.24";
 
   if (!window.__uchihaFetchInstrumented) {
     window.__uchihaFetchInstrumented = true;
