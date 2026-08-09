167beb740597536a3b12d91800166f093712cbc5
yaminuchiha1245-png <yaminuchiha1245@gmail.com>
2026-08-09 06:10:03 +0300

    chore(store): bump storefront asset release

--- builder/public/theme.js
+++ builder/public/theme.js
@@ -1,8 +1,8 @@
 (function () {
   "use strict";
 
-  var RELEASE = "2026.08.08.24-color";
-  var ASSET_VERSION = "2026.08.08.24";
+  var RELEASE = "2026.08.09.1-color";
+  var ASSET_VERSION = "2026.08.09.1";
   var storageKey = "uchiha-ui-theme";
   var root = document.documentElement;
   var media = typeof window.matchMedia === "function"
