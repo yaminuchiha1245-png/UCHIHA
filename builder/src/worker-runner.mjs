718c637bc5a3d73bbe24a5836f13994431e45924
yaminuchiha1245-png <yaminuchiha1245@gmail.com>
2026-08-14 12:12:01 +0300

    Run automatic subscription expiry in worker

--- builder/src/worker-runner.mjs
+++ builder/src/worker-runner.mjs
@@ -1,11 +1,14 @@
 import { createRuntime } from "./runtime.mjs";
+import { startSubscriptionExpiryLoop } from "./subscription-expiry.mjs";
 import { startWorkerLoop } from "./worker.mjs";
 
 const { config, db } = await createRuntime();
-const stop = startWorkerLoop(db, config, console, 750);
+const stopWorker = startWorkerLoop(db, config, console, 750);
+const stopSubscriptionExpiry = startSubscriptionExpiryLoop(db, console, 60_000);
 
 async function shutdown() {
-  stop();
+  stopSubscriptionExpiry();
+  stopWorker();
   await db.close();
   process.exit(0);
 }
@@ -13,4 +16,3 @@ async function shutdown() {
 process.once("SIGINT", shutdown);
 process.once("SIGTERM", shutdown);
 console.log("UCHIHA Builder worker is running");
-
