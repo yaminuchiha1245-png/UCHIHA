e6ef22a6bc36a8876b28cfd6c11b4648055dcdfe
yaminuchiha1245-png <yaminuchiha1245@gmail.com>
2026-08-15 11:01:05 +0300

    Remove obsolete smoke staging wrapper

--- builder/scripts/smoke-vps-strict.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Strict smoke snapshot. The deploy-facing wrapper lives in smoke-vps.sh.
-# This file is populated from the previously enforced smoke in the following commit.
-exit 86
