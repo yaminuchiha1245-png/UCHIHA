5511ce992910e56d5ec11653c9c3d0c6dd0a0d07
yaminuchiha1245-png <yaminuchiha1245@gmail.com>
2026-09-05 06:27:25 +0300

    Bump Control Center to alpha09

--- control-center/android-apk/app/build.gradle.kts
+++ control-center/android-apk/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.uchiha.controlcenter"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "2.0.0-alpha08"
+        versionCode = 10
+        versionName = "2.0.0-alpha09"
         buildConfigField("String", "API_BASE_URL", "\"https://panel.uchiha-builder.com/api/mobile\"")
     }
 
